e22d3fd36b18c980825a39116368948567f50922
Bjorn Runaker <bjorn@runaker.se>
2021-05-09 15:13:10 +0200

    improve data extract

--- convert.py
+++ convert.py
@@ -4,15 +4,19 @@ from pathlib import Path
 import natsort
 import re
 import json
-from collections import namedtuple
 import csv
 
 
 def _preprocess_bw_data(src_path: str, dst_path: str) -> int:
+    if os.path.exists(dst_path):
+        if os.stat(dst_path).st_size > 1024 * 1024:
+            print("{dst} exists with size {size}, skip calculation".format(dst=dst_path,
+                                                                           size=os.stat(dst_path).st_size))
+            return 0
+
     pathlist = Path(src_path).glob('**/*.json')
 
     list1 = []
-    list2 = []
     for path in pathlist:
         # because path is object not string
         path_in_str = str(path)
@@ -23,7 +27,8 @@ def _preprocess_bw_data(src_path: str, dst_path: str) -> int:
     with open(dst_path, 'w', newline='') as file_out:
         csv_out = csv.writer(file_out)
         csv_out.writerow(["year", "month", "day", "hour", "minute", "second", "lte_modem_latency", "5G_modem_latency",
-                          "5G_mobil_latency", "IP", "bw_down", "bw_up", "lat", "lon", "ping", "test_server"])
+                          "5G_mobil_latency", "IP", "bw_down", "bw_up", "lat", "lon", "ping",
+                          "test_server", "client_lat", "client_lon"])
 
         row = 0
 
@@ -54,31 +59,33 @@ def _preprocess_bw_data(src_path: str, dst_path: str) -> int:
                                 bw_awail = True
                             except ValueError:
                                 pass
+                        if data[0]["eno1"]["latency"] > 0 or data[1]["usb0"]["latency"] > 0 or \
+                                data[2]["usb1"]["latency"] > 0:
+                            if len(data) > 3:
+                                ip = data[3].get("ip", "")
                             else:
-                                if data[0]["eno1"]["latency"] > 0 or data[1]["usb0"]["latency"] > 0 or data[2]["usb1"]["latency"] > 0:
-                                    if len(data) > 3:
-                                        IP = data[3].get("ip", "")
-                                    else:
-                                        IP = ""
-                                    if bw_awail:
-                                        bw_down = int(data[4]["bandwidth"]["download"])
-                                        bw_up = int(data[4]["bandwidth"]["upload"])
-                                        bw_srv_lat = float(data[4]["bandwidth"]["server"]["lat"])
-                                        bw_srv_lon = float(data[4]["bandwidth"]["server"]["lon"])
-                                        bw_ping = float(data[4]["bandwidth"]["ping"])
-                                        bw_host = data[4]["bandwidth"]["server"]["host"]
-                                    else:
-                                        bw_down = bw_up = 0
-                                        bw_srv_lat = bw_srv_lon = bw_ping = 0
-                                        bw_host = ""
+                                ip = ""
+                            if bw_awail:
+                                bw_down = int(data[4]["bandwidth"]["download"])
+                                bw_up = int(data[4]["bandwidth"]["upload"])
+                                bw_srv_lat = float(data[4]["bandwidth"]["server"]["lat"])
+                                bw_srv_lon = float(data[4]["bandwidth"]["server"]["lon"])
+                                bw_client_lat = float(data[4]["bandwidth"]["client"]["lat"])
+                                bw_client_lon = float(data[4]["bandwidth"]["client"]["lon"])
+                                bw_ping = float(data[4]["bandwidth"]["ping"])
+                                bw_host = data[4]["bandwidth"]["server"]["host"]
+                            else:
+                                bw_down = bw_up = 0
+                                bw_client_lat = bw_client_lon = bw_srv_lat = bw_srv_lon = bw_ping = 0
+                                bw_host = ""
 
-                                    csv_out.writerow(
-                                        [year, month, day, hour, minutes, seconds, float(data[0]["eno1"]["latency"]),
-                                         float(data[1]["usb0"]["latency"]), float(data[2]["usb1"]["latency"]),
-                                         IP, bw_down, bw_up, bw_srv_lat,
-                                         bw_srv_lon, bw_ping, bw_host])
+                            csv_out.writerow(
+                                [year, month, day, hour, minutes, seconds, float(data[0]["eno1"]["latency"]),
+                                 float(data[1]["usb0"]["latency"]), float(data[2]["usb1"]["latency"]),
+                                 ip, bw_down, bw_up, bw_srv_lat,
+                                 bw_srv_lon, bw_ping, bw_host, bw_client_lat, bw_client_lon])
 
-                                    row += 1
+                            row += 1
     return row
 
 
